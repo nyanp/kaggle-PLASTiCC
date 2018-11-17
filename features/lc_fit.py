0f1f5871c690a48549ae3f92205be415e819f964
nyanp-study <nyanpn@gmail.com>
2018-11-17 20:51:27 +0900

    lc fitting

---
+++ features/lc_fit.py
@@ -0,0 +1,78 @@
+import sys
+import sncosmo
+from sncosmo.models import Model
+import pandas as pd
+import time
+from astropy.table import Table
+from contextlib import contextmanager
+from tqdm import tqdm
+
+training_only = False
+
+@contextmanager
+def timer(name):
+    s = time.time()
+    yield
+
+    print('[{}] {}'.format(time.time() - s, name))
+
+
+def fitting(model, meta, data, object_id):
+    z = meta.loc[object_id, 'hostgal_photoz']
+    zerr = meta.loc[object_id, 'hostgal_photoz_err']
+    table = Table.from_pandas(data[data.object_id == object_id])
+
+    # run the fit
+    result, fitted_model = sncosmo.fit_lc(
+        table, model,
+        ['z', 't0', 'x0', 'x1', 'c'],  # parameters of model to vary
+        bounds={'z': (z - zerr, z + zerr)})  # bounds on parameters (if any)
+
+    return [result.chisq] + [result.ncall] + list(result.parameters) + list(result.errors.values())
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        raise RuntimeError('Specify Data Index')
+
+    data_index = int(sys.argv[1])
+
+    print('index: {}'.format(data_index))
+    with timer('load data'):
+        meta = pd.read_feather('../input/meta.f')
+        if training_only:
+            meta = meta[~meta.target.isnull()]
+        meta = meta[meta.hostgal_photoz > 0].reset_index(drop=True)
+        meta.set_index('object_id', inplace=True)
+
+        lc = pd.read_feather('../input/all_{}.f'.format(data_index))
+        if training_only:
+            lc = lc[lc.object_id.isin(meta.index)].reset_index(drop=True)
+
+    print('shape: {}'.format(lc.shape))
+
+    passbands = ['lsstu','lsstg','lsstr','lssti','lsstz','lssty']
+
+    with timer('prep'):
+        s = time.time()
+        lc['band'] = lc['passband'].apply(lambda x: passbands[x])
+        lc['zpsys'] = 'ab'
+        lc['zp'] = 25.0
+
+    ret = pd.DataFrame(
+        columns=['chisq', 'ncall', 'z', 't0', 'x0', 'x1', 'c', 'z_err', 't0_err', 'x0_err', 'x1_err', 'c_err'])
+
+    # create a model
+    model = Model(source='salt2')
+
+    n_errors = 0
+
+    for i in tqdm(range(len(meta))):
+        object_id = meta.index[i]
+        try:
+            ret.loc[object_id] = fitting(model, meta, lc, object_id)
+        except:
+            n_errors += 1
+            pass
+
+    print('total {} data processed. {} data was skipped'.format(len(meta), n_errors))
--- run_model.py
+++ run_model.py
@@ -1,9 +1,9 @@
 
 from experiments.experiment8 import *
 from experiments.experiment9 import *
-from experiments.experiment27 import *
+from experiments.experiment28 import *
 from experiments.experiment1 import Experiment1
 
-exp = Experiment27(basepath='./')
+exp = Experiment28(basepath='./')
 
 exp.execute()
