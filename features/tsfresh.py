e71d238eba7cceda7f88f7e72fb6e58de30108eb
nyanp-study <nyanpn@gmail.com>
2018-11-23 14:19:24 +0900

    tsfresh feature - split

--- features/tsfresh.py
+++ features/tsfresh.py
@@ -33,33 +33,35 @@ fcp = {
     },
 }
 
-lc = pd.read_feather('../input/all.f')
-meta = pd.read_feather('../input/meta.f')
-
 from tsfresh.feature_extraction import extract_features
 
-print('flux-passband')
-agg_df_ts_flux_passband = extract_features(lc,
-                                           column_id='object_id',
-                                           column_sort='mjd',
-                                           column_kind='passband',
-                                           column_value='flux',
-                                           default_fc_parameters=fcp['flux_passband'], n_jobs=12)
-agg_df_ts_flux_passband.reset_index().to_feather('../features/ts_flux_passband.f')
 
-print('flux')
-agg_df_ts_flux = extract_features(lc,
+for i in range(30):
+    lc = pd.read_feather('../input/all_{}.f'.format(i))
+    print('flux-passband')
+    agg_df_ts_flux_passband = extract_features(lc,
+                                               column_id='object_id',
+                                               column_sort='mjd',
+                                               column_kind='passband',
+                                               column_value='flux',
+                                               default_fc_parameters=fcp['flux_passband'], n_jobs=12)
+    agg_df_ts_flux_passband.reset_index().to_feather('../features/ts_flux_passband_{}.f'.format(i))
+
+for i in range(30):
+    print('flux')
+    lc = pd.read_feather('../input/all_{}.f'.format(i))
+    agg_df_ts_flux = extract_features(lc,
+                                      column_id='object_id',
+                                      column_value='flux',
+                                      default_fc_parameters=fcp['flux'], n_jobs=12)
+    agg_df_ts_flux.reset_index().to_feather('../features/ts_flux_{}.f'.format(i))
+
+for i in range(30):
+    print('mjd')
+    lc = pd.read_feather('../input/all_{}.f'.format(i))
+    df_det = lc[lc['detected']==1].copy()
+    agg_df_mjd = extract_features(df_det,
                                   column_id='object_id',
-                                  column_value='flux',
-                                  default_fc_parameters=fcp['flux'], n_jobs=12)
-agg_df_ts_flux.reset_index().to_feather('../features/ts_flux.f')
-
-print('mjd')
-df_det = lc[lc['detected']==1].copy()
-agg_df_mjd = extract_features(df_det,
-                              column_id='object_id',
-                              column_value='mjd',
-                              default_fc_parameters=fcp['mjd'], n_jobs=12)
-
-
-agg_df_mjd.reset_index().to_feather('../features/ts_mjd.f')
+                                  column_value='mjd',
+                                  default_fc_parameters=fcp['mjd'], n_jobs=12)
+    agg_df_mjd.reset_index().to_feather('../features/ts_mjd_{}.f'.format(i))
