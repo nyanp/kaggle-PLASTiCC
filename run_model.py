9cc29a1ad9874d5a792e901ccd122d1363d1a73e
nyanp-study <nyanpn@gmail.com>
2018-11-24 14:41:37 +0900

    experiment41 - LB 0.882

--- run_model.py
+++ run_model.py
@@ -1,9 +1,9 @@
 
 from experiments.experiment8 import *
 from experiments.experiment9 import *
-from experiments.experiment40 import *
+from experiments.experiment41 import *
 from experiments.experiment1 import Experiment1
 
-exp = Experiment40(basepath='./')
+exp = Experiment41(basepath='./')
 
 exp.execute()
