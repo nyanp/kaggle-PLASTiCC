94d304fc5c6498cf6fb3399f7cb41710b7239539
nyanp-study <nyanpn@gmail.com>
2018-12-06 01:09:41 +0900

    new type of template (SNANA SNIb)

--- run_model.py
+++ run_model.py
@@ -1,26 +1,9 @@
 from experiments.experiment50 import *
 from experiments.experiment51 import *
 from experiments.experiment53 import *
-from experiments.experiment56 import *
+from experiments.experiment57 import *
 
 
-exp = Experiment56(basepath='./',
-                   submit_path='output/experiment56.csv',
-                   log_name='experiment56',
-                   use_extra_classifier=False)
-exp.execute()
-
-
-exp = Experiment56(basepath='./',
-                   submit_path='output/experiment56_th975.csv',
-                   log_name='experiment56_th975',
-                   use_extra_classifier=False,
-                   pseudo_th=0.975)
-exp.execute()
-
-exp = Experiment56(basepath='./',
-                   submit_path='output/experiment56_th98.csv',
-                   log_name='experiment56_th98',
-                   use_extra_classifier=False,
-                   pseudo_th=0.98)
+exp = Experiment57(basepath='./',
+                   log_name='experiment57')
 exp.execute()
