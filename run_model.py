254648dfca2a537561e842fd2d225a5f7d0a873e
nyanp-study <nyanpn@gmail.com>
2018-12-16 00:35:04 +0900

    set round=500

--- run_model.py
+++ run_model.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     print('Training start. seed: {}'.format(seed))
     #exp = Experiment71(basepath='./', seed=seed, submit_path='experiment71_seed{}.csv'.format(seed),
     #                   use_extra_classifier=False, n_estimators_extra_classifier=None)
-    exp = Experiment71(basepath='./', seed=seed, submit_path='experiment71_seed{}.csv'.format(seed), log_name='experiment71_pl',
-                       use_extra_classifier=True, n_estimators_extra_classifier=1000, use_pl_labels=True)
+    exp = Experiment71(basepath='./', seed=seed, submit_path='experiment71_r500_seed{}.csv'.format(seed), log_name='experiment71_pl',
+                       use_extra_classifier=True, n_estimators_extra_classifier=500, use_pl_labels=True)
     exp.execute()
